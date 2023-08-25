6e681dcab0f3b317e38d709b1e4edd180129ce47
Clifford-arch <cliffordchristophermarx@gmail.com>
2023-08-26 01:05:53 +0530

    Step 1, Step 2, Step 3, Step 4 and Step 5: Environment setup, Layout, Supabase setup, Supabase types, Providers for Auth and Supabase.

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Figtree } from 'next/font/google'
+import Sidebar from '@/components/Sidebar'
+import SupabaseProvider from '@/providers/SupabaseProvider'
+import UserProvider from '@/providers/UserProvider'
+const font = Figtree({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'HarmoniX',
+  description: 'Listen to your vibe',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+          <SupabaseProvider>
+            <UserProvider> 
+              <Sidebar>
+                {children}
+              </Sidebar>
+            </UserProvider>
+          </SupabaseProvider>
+        </body>
+    </html>
+  )
+}
