e187045828541812cc5f523b2933d90f2b1e55fe
Clifford-arch <cliffordchristophermarx@gmail.com>
2023-09-01 20:39:00 +0530

    Step 10: Player functionality done.

--- app/(site)/components/PageContent.tsx
+++ app/(site)/components/PageContent.tsx
@@ -1,24 +1,27 @@
 "use client";
 
-import SongItem from "@/components/SongItem";
 import { Song } from "@/types";
-
+import useOnPlay from "@/hooks/useOnPlay";
+import SongItem from "@/components/SongItem";
 
 interface PageContentProps {
-    songs: Song[];
-  }
-  
+  songs: Song[];
+}
+
 const PageContent: React.FC<PageContentProps> = ({
-    songs
-})=> {
-    if (songs.length === 0) {
-        return (
-          <div className="mt-4 text-neutral-400">
-            No songs available.
-          </div>
-        )
-      }
-  return (
+  songs
+}) => {
+  const onPlay = useOnPlay(songs);
+
+  if (songs.length === 0) {
+    return (
+      <div className="mt-4 text-neutral-400">
+        No songs available.
+      </div>
+    )
+  }
+
+  return ( 
     <div 
       className="
         grid 
@@ -33,14 +36,14 @@ const PageContent: React.FC<PageContentProps> = ({
       "
     >
       {songs.map((item) => (
-        <SongItem
-        onClick={()=>{}}
-        key={item.id} 
-        data={item}
-      />  
+        <SongItem 
+          onClick={(id: string) => onPlay(id)} 
+          key={item.id} 
+          data={item}
+        />
       ))}
     </div>
-  )
+  );
 }
-
-export default PageContent
+ 
+export default PageContent;
