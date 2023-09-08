12e5d20515b20e6b2688007cac4123af7e884158
Clifford-arch <cliffordchristophermarx@gmail.com>
2023-09-08 15:29:52 +0530

    Step 11: Stripe Integration Done.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,41 +1,39 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Figtree } from 'next/font/google'
-import Sidebar from '@/components/Sidebar'
-import SupabaseProvider from '@/providers/SupabaseProvider'
-import UserProvider from '@/providers/UserProvider'
-import ModalProvider from '@/providers/ModalProvider'
-import ToasterProvider from '@/providers/ToasterProvider'
-import getSongsByUserId from '@/actions/getSongsByUserId'
-import Player from '@/components/Player'
-const font = Figtree({ subsets: ['latin'] })
+import "./globals.css";
+import type { Metadata } from "next";
+import { Figtree } from "next/font/google";
+import Sidebar from "@/components/Sidebar";
+import SupabaseProvider from "@/providers/SupabaseProvider";
+import UserProvider from "@/providers/UserProvider";
+import ModalProvider from "@/providers/ModalProvider";
+import ToasterProvider from "@/providers/ToasterProvider";
+import getSongsByUserId from "@/actions/getSongsByUserId";
+import Player from "@/components/Player";
+const font = Figtree({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'HarmoniX',
-  description: 'Listen to your vibe',
-}
+  title: "HarmoniX",
+  description: "Listen to your vibe",
+};
 
-export const revalidate=0;
+export const revalidate = 0;
 export default async function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
-  const userSongs= await getSongsByUserId();
+  const userSongs = await getSongsByUserId();
   return (
     <html lang="en">
       <body className={font.className}>
-        <ToasterProvider/>
-          <SupabaseProvider>
-            <UserProvider> 
-              <ModalProvider/>
-              <Sidebar songs={userSongs}>
-                {children}
-              </Sidebar>
-              <Player/>
-            </UserProvider>
-          </SupabaseProvider>
-        </body>
+        <ToasterProvider />
+        <SupabaseProvider>
+          <UserProvider>
+            <ModalProvider />
+            <Sidebar songs={userSongs}>{children}</Sidebar>
+            <Player />
+          </UserProvider>
+        </SupabaseProvider>
+      </body>
     </html>
-  )
+  );
 }
--- components/SideBarItem.tsx
+++ components/SideBarItem.tsx
@@ -7,13 +7,16 @@ interface SideBarItemProps{
     label:string;
     active?:boolean;
     href:string;
+    
 }
 const SideBarItem: React.FC<SideBarItemProps> = ({
     icon:Icon,label,active,href
 }) => {
+  
   return (
     <Link
     href={href}
+    
     className={twMerge("flex flex-row h-auto items-center w-full gap-x-4 text-md font-medium cursor-pointer hover:text-white transition text-neutral-400 py-1", active && "text-white" )}
     ><Icon size={26}></Icon><p className="truncate w-full">{label}</p></Link>
   )
